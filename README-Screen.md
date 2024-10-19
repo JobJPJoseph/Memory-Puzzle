54a3841240c26d9dade2301609496e67f41ed622
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-10-19 08:12:50 -0400

    completed Screen.fillGrid

--- README-Screen.md
+++ README-Screen.md
@@ -2,3 +2,8 @@
 
 ## Tasks
     We need to make an grid. However, we are not going to use arrays but use a Hash Table to create this grid. This will allow us to save time at the cost of space. We will use the coordinates as keys and letters as values. In total for Each letter should appear twice in the object.
+
+    1. Create a Hash Table called `realCoordinates`.
+    2. Create a Hash Table called `printedCoordinates`.
+    3. Should a create method called `fillGrid`
+    4. Create a constructor. This here is to set up the important data of the project.
--- test/screen.spec.js
+++ test/screen.spec.js
@@ -9,4 +9,51 @@ describe('Screen', function () {
         expect(Screen).to.exist;
     });
 
+    let screen;
+
+    beforeEach(function () {
+        screen = new Screen();
+    });
+
+    it('should create an empty object called coordinates', function () {
+        expect(Screen.grid).to.a('object');
+    });
+
+    describe('fillGrid', function () {
+
+        it(`should fill Screen.coordinates with 8 pairs letters from the alphabet`, function () {
+            Screen.fillCoordinates();
+            Screen.fillGrid();
+
+            let pairs = {};
+
+
+
+            for (let i = 0; i < Screen.grid.data.length; i++) {
+                let node = Screen.grid.data[i];
+
+                if (node) {
+
+                    while (node) {
+
+                        if (pairs[node.value]) {
+                            pairs[node.value]++;
+                        } else {
+                            pairs[node.value] = 1;
+                        }
+                        node = node.next;
+                    }
+
+                }
+
+            }
+
+            for (let letter in pairs) {
+                expect(pairs[letter]).to.equal(2);
+            }
+
+        });
+
+    });
+
 });
