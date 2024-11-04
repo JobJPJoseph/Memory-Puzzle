8ac13b4689323899ea0c506462f2a2dd51735073
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-11-04 11:47:11 -0500

    we found the issue in mySet and currently working on Screen.fillGrid()

--- test/screen.spec.js
+++ test/screen.spec.js
@@ -67,6 +67,8 @@ describe('Screen', function () {
     describe('fillAlphabetSet' , function () {
 
         it('should fill the set with letters from the alphabet', function () {
+            Screen.fillAlphabetSet();
+
             let alphaStr = 'abcdefghijklmnopqrstuvwxyz';
 
             for (let i = 0; i < alphaStr.length; i++) {
@@ -81,7 +83,9 @@ describe('Screen', function () {
 
     describe('fillCoordinateArray', function () {
 
-        it('should fill the array that represents a 4 x 6 grid equalling 24  total and 12 pairs', function () {
+        it('should fill the array that represents a 6 x 4 grid equalling 24  total and 12 pairs', function () {
+            Screen.fillCoordinateArray();
+
             let array = [];
             const rowMax = 6;
             const colMax = 4;
@@ -101,7 +105,7 @@ describe('Screen', function () {
             for (let i = 0; i < rowMax; i++) {
 
                 for (let j = 0; j < colMax; j++) {
-                    let mine = JSON.stringify({ row: i, column: j });
+                    let mine = array[i][j];
                     let theres = Screen.availableCoordinates[i][j];
 
                     expect(mine === theres).to.be.true;
@@ -116,18 +120,25 @@ describe('Screen', function () {
     describe('fillGrid', function () {
 
         it('Each letter of the alphabet should have two coordinates', function () {
+            Screen.fillGrid();
 
-            for (let i = 0; i < Screen.gridHashTable.length; i++) {
-                let node = Screen.gridHashTable[i];
+            expect(Screen.gridHashTable.count > 1).to.be.true;
+            console.log(Screen.gridHashTable);
+
+            expect(Screen.gridHashTable.read("a").length > 1).to.be.true;
+
+            for (let i = 0; i < Screen.gridHashTable.data.length; i++) {
+                let node = Screen.gridHashTable.data[i];
 
                 if (node) {
 
-                    expect(Screen.alphabetSet.has(node.key));
-                    expect(node.value).to.be.instanceOf('array');
-                    expect(node.value.length).to.equal(2);
+                    expect(Screen.alphabetSet.has(node.key), 'if it has the key').to.be.true;
+                    expect(node.value, 'is the value an array').to.be.instanceOf('array');
+                    expect(node.value.length, 'is the array a length of 2').to.equal(2);
                 }
 
             }
+
         });
 
     });
